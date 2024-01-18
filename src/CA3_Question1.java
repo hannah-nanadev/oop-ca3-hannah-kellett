41e4ced94db3ae2403e1911a7e0e0782bf22ef70
hannah-nanadev <hannahkellett2@gmail.com>
2024-01-18 10:59:52 +0000

    Initialised stacks, made basic program loop that can be exited (but no stack manipulation functionality yet)

--- src/CA3_Question1.java
+++ src/CA3_Question1.java
@@ -1,12 +1,40 @@
 /**
- *  Name:
- *  Class Group:
+ *  Name: Hannah Kellett
+ *  Class Group: GD2B
  */
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
 public class CA3_Question1
 {
     public static void runSimulation()
     {
+        Deque<Integer> driveway = new ArrayDeque<>();
+        Deque<Integer> street = new ArrayDeque<>();
 
+        boolean running = true;
+        while(running)
+        {
+            System.out.println(
+                    "Driveway: " + driveway.toString()
+                + "\nStreet: " + street.toString()
+                + "\nWhat do you want to do?"
+                + "\nType in a positive integer to add a car or a negative integer to remove it."
+                + "\nType 'STOP' to stop."
+            );
+
+            Scanner input = new Scanner(System.in);
+
+            String in = input.nextLine();
+            if(in.equals("STOP"))
+            {
+                running = false;
+                System.out.println("Simulation stopping. Thank you!");
+            }
+
+        }
     }
 
     public static void main(String[] args) {
