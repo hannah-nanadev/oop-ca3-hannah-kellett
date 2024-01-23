87ca258dd0625373b350f1860823cbd9cf713c8b
hannah-nanadev <hannahkellett2@gmail.com>
2024-01-23 15:53:56 +0000

    Added functionality for reading a java file, getting identifiers and printing them out alongside lists of line numbers

--- src/CA3_Question3.java
+++ src/CA3_Question3.java
@@ -1,7 +1,8 @@
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.Map;
 import java.util.TreeMap;
+import java.io.File;
+import java.util.ArrayList;
 /**
  *  Name: Hannah Kellett
  *  Class Group: GD2B
@@ -9,14 +10,45 @@ import java.util.TreeMap;
 
 public class CA3_Question3
 {
-    public static void readFile(String fileName)
-    {
+    public static TreeMap<String, ArrayList<Integer>> readFile(String fileName) throws FileNotFoundException {
+        File file = new File(fileName);
+        TreeMap<String, ArrayList<Integer>> identifiers = new TreeMap<>();
 
+        try {
+            Scanner line = new Scanner(file);
+            int lineNo = 1;
+
+            while(line.hasNextLine())
+            {
+                String current = line.nextLine();
+
+                Scanner in = new Scanner(current);
+                in.useDelimiter("[^A-Za-z0-9_]+");
+
+                while(in.hasNext())
+                {
+                    String id = in.next(); //Get next identifier in line
+
+                    if(!identifiers.containsKey(id)) //Add entry to map if not there before adding line numbers
+                    {
+                        identifiers.put(id, new ArrayList<>());
+                    }
+                    identifiers.get(id).add(lineNo);
+                }
+
+                lineNo++;
+
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        return identifiers;
     }
 
     public static void main(String[] args) throws FileNotFoundException {
-        TreeMap<Integer, String> identifiers = new TreeMap<>();
+        TreeMap<String, ArrayList<Integer>> identifiers = readFile("src/CA3_Question1.java");
+        System.out.println(identifiers);
 
-        readFile("src/CA3_Question1.java");
     }
 }
