974c614c102c1ee09d70e6467dbec7f35dabd63e
hannah-nanadev <hannahkellett2@gmail.com>
2024-01-30 15:47:49 +0000

    Added functionality for selling shares across multiple companies
    Appears to be slightly buggier than question 6, which a lot of the code was lifted and modified from - will go back and fix later

--- src/CA3_Question7.java
+++ src/CA3_Question7.java
@@ -1,7 +1,7 @@
 import java.util.*;
 /**
- *  Name:
- *  Class Group:
+ *  Name: Hannah Kellett
+ *  Class Group: GD2B
  */
 public class CA3_Question7
 {
@@ -15,6 +15,8 @@ public class CA3_Question7
     */
     public static void main(String[] args) {
 
+        Map<String, Queue<Share>> shares = new HashMap<>();
+
         Scanner in = new Scanner(System.in);
         String command="";
         do {
@@ -26,6 +28,14 @@ public class CA3_Question7
                 int qty = in.nextInt();
                 double price = in.nextDouble();
                 // Code to buy shares here
+
+                if(!shares.containsKey(company)) //Make new entry for company if not present before adding share to queue
+                {
+                    shares.put(company, new LinkedList<>());
+                }
+                shares.get(company).add(new Share(qty, price));
+
+
             }
             else if(command.equals("sell"))
             {
@@ -33,6 +43,52 @@ public class CA3_Question7
                 int qty = in.nextInt();
                 double price = in.nextDouble();
                 // Code to sell shares and calculate profit here
+
+                //First, check number of shares company has
+                int sharesTotal = 0;
+                for(int i = 0; i<shares.size(); i++)
+                {
+                    sharesTotal = sharesTotal+shares.get(company).peek().getQuantity();
+                }
+
+                //Following code adapted from question 7
+                double sellPrice = qty*price;
+                if(qty<=sharesTotal||!shares.get(company).isEmpty())
+                {
+                    int sold = qty;
+                    double profit;
+                    double buyPrices = 0;
+
+                    while(sold>0)
+                    {
+                        int topQty = shares.get(company).peek().getQuantity();
+
+                        if(sold<=topQty){
+                            buyPrices = buyPrices+(sold*shares.get(company).peek().getPrice());
+                            shares.get(company).peek().setQuantity(topQty-sold);
+                            sold = 0;
+                        }
+                        else
+                        {
+                            buyPrices = buyPrices+(shares.get(company).peek().getCost());
+                            sold = sold-shares.get(company).peek().getQuantity();
+                            shares.get(company).remove();
+                        }
+
+                        if(shares.get(company).peek().getQuantity()==0)
+                        {
+                            shares.get(company).remove();
+                        }
+                    }
+
+                    profit = sellPrice-buyPrices;
+                    System.out.println("Earnings from selling shares for " + company + ": " + profit);
+
+                }
+                else{
+                    System.out.println(company + " does not have that many shares.");
+                }
+
             }
         }while(!command.equalsIgnoreCase("quit"));
     }
