6da424cdd9bf0f147b20c4739b4c6a39a56f691e
hannah-nanadev <hannahkellett2@gmail.com>
2024-02-06 15:52:32 +0000

    began work on q10

--- src/CA3_Question10.java
+++ src/CA3_Question10.java
@@ -1,11 +1,24 @@
+import com.sun.source.tree.Tree;
+
+import java.util.TreeSet;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
 /**
- *  Name:
- *  Class Group:
+ *  Name: Hannah Kellett
+ *  Class Group: GD2B
  */
 public class CA3_Question10
 {
-
     public static void main(String[] args) {
+        Map<String, TreeSet<DistanceTo>> connections = genConnections();
+    }
 
+    public static Map<String, TreeSet<DistanceTo>> genConnections()
+    {
+        Map<String, TreeSet<DistanceTo>> map = new HashMap<>();
+
+        return map;
     }
 }
