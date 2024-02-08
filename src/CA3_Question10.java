063a86d0c1b22242399c0e4bd572c0a25afcc3da
hannah-nanadev <hannahkellett2@gmail.com>
2024-02-08 15:47:51 +0000

    bugged implementation of algorithm - unsure how to properly implement it

--- src/CA3_Question10.java
+++ src/CA3_Question10.java
@@ -1,7 +1,7 @@
 import com.sun.source.tree.Tree;
 
 import java.util.TreeSet;
-import java.util.HashMap;
+import java.util.TreeMap;
 import java.util.Map;
 import java.util.PriorityQueue;
 
@@ -17,12 +17,44 @@ public class CA3_Question10
 {
     public static void main(String[] args) {
         Map<String, TreeSet<DistanceTo>> connections = readConnections();
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter a city name: ");
+        String from = input.nextLine();
+
+        //Create and initialise priority queue
+        PriorityQueue<DistanceTo> queue = new PriorityQueue<>();
+        queue.add(new DistanceTo(from, 0));
+
+        Map<String, Integer> shortestKnownDistance = new TreeMap<>();
+
+        //Algorithm to add elements to shortestKnownDistance
+        while(!queue.isEmpty())
+        {
+            DistanceTo current = queue.poll();
+            String cTarget = current.getTarget();
+            if(!shortestKnownDistance.containsKey(cTarget))
+            {
+                //Add new target to map
+                shortestKnownDistance.put(cTarget, current.getDistance());
+
+                for(int i = 0; i<connections.get(cTarget).size(); i++)
+                {
+                    DistanceTo dConn = connections.get(cTarget).pollFirst();
+                    DistanceTo nConn = new DistanceTo(dConn.getTarget(),
+                            current.getDistance()+dConn.getDistance());
+                    queue.add(nConn);
+                }
+            }
+        }
+
+        System.out.println(shortestKnownDistance);
     }
 
     public static Map<String, TreeSet<DistanceTo>> readConnections()
     {
         File file = new File("citymap.txt");
-        Map<String, TreeSet<DistanceTo>> map = new HashMap<>();
+        Map<String, TreeSet<DistanceTo>> map = new TreeMap<>();
 
         try
         {
