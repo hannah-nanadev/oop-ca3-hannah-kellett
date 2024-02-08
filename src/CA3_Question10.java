f50deb75daefe78d41778496316dee6276981d88
hannah-nanadev <hannahkellett2@gmail.com>
2024-02-08 15:17:22 +0000

    replaced hardcoded map generation with file parser

--- src/CA3_Question10.java
+++ src/CA3_Question10.java
@@ -5,6 +5,10 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.PriorityQueue;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  *  Name: Hannah Kellett
  *  Class Group: GD2B
@@ -12,54 +16,38 @@ import java.util.PriorityQueue;
 public class CA3_Question10
 {
     public static void main(String[] args) {
-        Map<String, TreeSet<DistanceTo>> connections = genConnections();
+        Map<String, TreeSet<DistanceTo>> connections = readConnections();
     }
 
-    public static Map<String, TreeSet<DistanceTo>> genConnections()
+    public static Map<String, TreeSet<DistanceTo>> readConnections()
     {
+        File file = new File("citymap.txt");
         Map<String, TreeSet<DistanceTo>> map = new HashMap<>();
 
-        //Super scuffed but don't know how to do it better
-        map.put("Pendleton", new TreeSet<>());
-        map.get("Pendleton").add(new DistanceTo("Pierre", 2));
-        map.get("Pendleton").add(new DistanceTo("Pueblo", 8));
-        map.get("Pendleton").add(new DistanceTo("Phoenix", 4));
+        try
+        {
+            Scanner in = new Scanner(file);
+            String c1;
+            String c2;
+            int dist;
 
-        map.put("Pierre", new TreeSet<>());
-        map.get("Pierre").add(new DistanceTo("Pendleton", 2));
-        map.get("Pierre").add(new DistanceTo("Pueblo", 3));
+            while(in.hasNext())
+            {
+                c1 = in.next();
+                c2 = in.next();
+                dist = Integer.parseInt(in.next());
 
-        map.put("Pueblo", new TreeSet<>());
-        map.get("Pueblo").add(new DistanceTo("Pendleton", 8));
-        map.get("Pueblo").add(new DistanceTo("Pierre", 3));
-        map.get("Pueblo").add(new DistanceTo("Phoenix", 3));
-        map.get("Pueblo").add(new DistanceTo("Peoria", 3));
+                if(!map.containsKey(c1))
+                {
+                    map.put(c1, new TreeSet<>());
+                }
+                map.get(c1).add(new DistanceTo(c2, dist));
+            }
 
-        map.put("Phoenix", new TreeSet<>());
-        map.get("Phoenix").add(new DistanceTo("Pendleton", 4));
-        map.get("Phoenix").add(new DistanceTo("Pueblo", 3));
-        map.get("Phoenix").add(new DistanceTo("Peoria", 4));
-        map.get("Phoenix").add(new DistanceTo("Pittsburgh", 10));
-        map.get("Phoenix").add(new DistanceTo("Pensacola", 5));
-
-        map.put("Peoria", new TreeSet<>());
-        map.get("Peoria").add(new DistanceTo("Pueblo", 3));
-        map.get("Peoria").add(new DistanceTo("Pittsburgh", 5));
-        map.get("Peoria").add(new DistanceTo("Phoenix", 4));
-
-        map.put("Pittsburgh", new TreeSet<>());
-        map.get("Pittsburgh").add(new DistanceTo("Peoria", 5));
-        map.get("Pittsburgh").add(new DistanceTo("Pensacola", 4));
-        map.get("Pittsburgh").add(new DistanceTo("Princeton", 2));
-
-        map.put("Pensacola", new TreeSet<>());
-        map.get("Pensacola").add(new DistanceTo("Phoenix", 5));
-        map.get("Pensacola").add(new DistanceTo("Pittsburgh", 4));
-        map.get("Pensacola").add(new DistanceTo("Princeton", 5));
-
-        map.put("Princeton", new TreeSet<>());
-        map.get("Princeton").add(new DistanceTo("Pittsburgh", 2));
-        map.get("Princeton").add(new DistanceTo("Pensacola", 4));
+        } catch(IOException e)
+        {
+            System.out.println("File not found.");
+        }
 
         return map;
     }
